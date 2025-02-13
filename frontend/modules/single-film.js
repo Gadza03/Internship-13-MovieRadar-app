75440e05e28c6cad7a53e56f7600cc7139df9e1c
Gadza03 <mgadza@pmfst.hr>
2025-02-13 21:59:33 +0100

    Create film card with details

---
+++ frontend/modules/single-film.js
@@ -0,0 +1,16 @@
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active");
+    });
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+    tab.classList.add("active-tab");
+    target.classList.add("active");
+  });
+});
